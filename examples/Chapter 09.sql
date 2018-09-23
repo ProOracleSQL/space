5786e38024c90fff3477945db2185b158c68af67
Jon Heller <jonearles@yahoo.com>
2018-09-23 17:45:07 -0500

    More chapter 9 examples.

--- examples/Chapter 09.sql
+++ examples/Chapter 09.sql
@@ -217,3 +217,189 @@ alter table organization_parallel noparallel;
 --Run this concurrent with above statement to see parallelism.
 --(NOT SHOWN IN BOOK)
 select * from v$px_process;
+
+
+
+---------------------------------------------------------------------------
+-- Index
+---------------------------------------------------------------------------
+
+--Comparing linear and binary search.
+select * from launch where launch_id = :n;
+
+
+--Find index heights.
+select blevel+1 height, dba_indexes.*
+from dba_indexes;
+
+
+--Create bitmap index on low-cardinality columns.
+create bitmap index launch_category_idx
+on launch(launch_category);
+
+
+--Create another bitmap index and show BITMAP AND operation.
+--(NOT SHOWN IN BOOK.)
+create bitmap index launch_status_idx
+on launch(launch_status);
+
+explain plan for
+select *
+from launch
+where launch_status = 'failure'
+	and launch_category = 'orbital';
+
+select * from table(dbms_xplan.display);
+
+
+--Create function based index.
+create index launch_date_idx
+on launch(trunc(launch_date));
+
+select *
+from launch
+where trunc(launch_date) = date '1957-10-04';
+
+
+--Make indexes unusable or usable.
+alter index launch_date_idx unusable;
+alter index launch_date_idx rebuild;
+
+
+--Rebuild online, quickly, and then reset properties.
+alter index launch_date_idx rebuild online nologging parallel;
+alter index launch_date_idx logging;
+alter index launch_date_idx noparallel;
+
+
+
+---------------------------------------------------------------------------
+-- Partitioning
+---------------------------------------------------------------------------
+
+--Our target query to optimize.
+select * from launch where launch_category = 'orbital';
+
+
+--Crate poor man's partitioning on LAUNCH table.
+--Create a table for each LAUNCH_CATEGORY.
+--(Only partially shown in book.)
+create table launch_suborbital_rocket      as select * from launch where launch_category = 'suborbital rocket';
+create table launch_military_missile       as select * from launch where launch_category = 'military missile';
+create table launch_orbital                as select * from launch where launch_category = 'orbital';
+create table launch_atmospheric_rocket     as select * from launch where launch_category = 'atmospheric rocket';
+create table launch_suborbital_spaceplane  as select * from launch where launch_category = 'suborbital spaceplane';
+create table launch_test_rocket            as select * from launch where launch_category = 'test rocket';
+create table launch_deep_space             as select * from launch where launch_category = 'deep space';
+create table launch_ballistic_missile_test as select * from launch where launch_category = 'ballistic missile test';
+create table launch_sounding_rocket        as select * from launch where launch_category = 'sounding rocket';
+create table launch_lunar_return           as select * from launch where launch_category = 'lunar return';
+
+--Create a view that combines per-category tables together.
+create or replace view launch_all as
+select * from launch_suborbital_rocket      where launch_category = 'suborbital rocket' union all
+select * from launch_military_missile       where launch_category = 'military missile' union all
+select * from launch_orbital                where launch_category = 'orbital' union all
+select * from launch_atmospheric_rocket     where launch_category = 'atmospheric rocket' union all
+select * from launch_suborbital_spaceplane  where launch_category = 'suborbital spaceplane' union all
+select * from launch_test_rocket            where launch_category = 'test rocket' union all
+select * from launch_deep_space             where launch_category = 'deep space' union all
+select * from launch_ballistic_missile_test where launch_category = 'ballistic missile test' union all
+select * from launch_sounding_rocket        where launch_category = 'sounding rocket' union all
+select * from launch_lunar_return           where launch_category = 'lunar return';
+
+--To quickly compare these, run with autotrace on and notice how the
+--LAUNCH_ALL view uses significantly less "consistent gets", and has
+--many "FILTER" operations in the execution plan.
+--This means Oracle is only selecting from the relevant table.
+set autotrace on;
+select /*+ full(launch) */ count(*) from launch where launch_category = 'orbital';
+select count(*) from launch_all where launch_category = 'orbital';
+
+
+--These return the same results, but LAUNCH_ALL is faster.
+select * from launch where launch_category = 'orbital';
+select * from launch_all where launch_category = 'orbital';
+
+
+--Create and query a partitioned launch table.
+create table launch_partition
+partition by list(launch_category)
+(
+	partition p_sub values('suborbital rocket'),
+	partition p_mil values('military missile'),
+	partition p_orb values('orbital'),
+	partition p_atm values('atmospheric rocket'),
+	partition p_pln values('suborbital spaceplane'),
+	partition p_tst values('test rocket'),
+	partition p_dep values('deep space'),
+	partition p_bal values('ballistic missile test'),
+	partition p_snd values('sounding rocket'),
+	partition p_lun values('lunar return')
+) as
+select * from launch;
+
+select * from launch_partition where launch_category = 'orbital';
+
+
+--Partition administration examples.
+delete from launch_partition partition (p_orb);
+alter table launch_partition truncate partition p_orb;
+
+
+
+---------------------------------------------------------------------------
+-- View
+---------------------------------------------------------------------------
+
+--View with "OR REPLACE" and "FORCE".
+create or replace force view bad_view as
+select * from does_not_exist;
+
+--The view exists but is broken and throws this error:
+--ORA-04063: view "JHELLER.BAD_VIEW2" has errors
+select * from bad_view2;
+
+
+--Create nested views and expand them.
+create or replace view view1 as select 1 a, 2 b from dual;
+create or replace view view2 as select a from view1;
+
+declare
+    v_output clob;
+begin
+    dbms_utility.expand_sql_text('select * from view2', v_output);
+    dbms_output.put_line(v_output);
+end;
+/
+
+SELECT "A1"."A" "A" FROM  (SELECT "A2"."A" "A" FROM  (SELECT 1 "A",2 "B" FROM "SYS"."DUAL" "A3") "A2") "A1"
+
+
+
+---------------------------------------------------------------------------
+-- User
+---------------------------------------------------------------------------
+
+--Create an application user account.
+create user application_user
+identified by "ridiculouslyLongPW52733042#$%^"
+--Schema account option:
+--  account lock
+--18c schema account option:
+--  no authentication
+profile application_profile
+default tablespace my_application_tablespace
+quota unlimited on my_application_tablespace;
+
+
+--Drop user and all its objects and privileges.
+--drop user application_user cascade;
+
+
+
+---------------------------------------------------------------------------
+-- Sequence
+---------------------------------------------------------------------------
+
+
