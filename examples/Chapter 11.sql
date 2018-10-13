de6a530cce4d2d757f9441007fe36e4835240fa3
Jon Heller <jonearles@yahoo.com>
2018-10-13 11:00:55 -0500

    Added Chapter 11 examples.

--- examples/Chapter 11.sql
+++ examples/Chapter 11.sql
@@ -1,23 +1,18 @@
 ---------------------------------------------------------------------------
--- 
+-- Comments
 ---------------------------------------------------------------------------
 
-
-/*
- *  _______        _       +------------------+
- * |__   __|      | |      |doesn't have to be+-------+ 
- *    | | _____  _| |_     +------------------+       |
- *    | |/ _ \ \/ / __|                               |
- *    | |  __/>  <| |_     +------+                   |
- *    |_|\___/_/\_\\__|    |boring+<------------------+
- *                         +------+
-*/
+--Hint and comment example.
+select /*+ cardinality(launch, 9999999) - I know this is a bad idea but...  */
+* from launch;
 
 
+--Don't use a comment after a line terminator.
+SQL> select * from dual; --This doesn't work.
+  2
 
 
-
-
+SQL> --Example of bad comment.
 SQL> select 1 from dual;
 
          1
@@ -31,43 +26,61 @@ SQL> /*a*/
          1
 
 
+
+/***********************************************************
+ *
+ *  _______        _       +------------------+
+ * |__   __|      | |      |doesn't have to be+-------+ 
+ *    | | _____  _| |_     +------------------+       |
+ *    | |/ _ \ \/ / __|                               |
+ *    | |  __/>  <| |_     +------+                   |
+ *    |_|\___/_/\_\\__|    |boring+<------------------+
+ *                         +------+
+ *
+***********************************************************/
 ;
 
-select * from dual;
 
-/* Text
-	select * from dual;
-	/
-	/
-*/
+---------------------------------------------------------------------------
+-- Choose Good Names
+---------------------------------------------------------------------------
 
-
-declare
-	v_test varchar2(100) := '
-	/
-	';
-begin
-	null;
-end;
-/
+--Avoid case sensitive names.
+select *
+from
+(
+	select 1 "Good luck using this name!"
+	from dual
+)
+where "Good luck using this name!" = 1;
 
 
+--Example of using values in the names.
+--(NOT SHOWN IN BOOK.)
+select launch_id, launch_date
+from
+(
+	select launch_id, launch_date,
+		row_number() over
+		(
+			partition by to_char(launch_date, 'YYYY')
+			order by launch_date
+		) first_when_1
+	from launch
+	where launch_category in ('orbital', 'deep space')
+)
+where first_when_1 = 1;
 
-SQL> declare
-  2     v_test varchar2(100) := '
-  3     /
-ERROR:
-ORA-01756: quoted string not properly terminated
+
+--Change table and column names.
+create table bad_table_name(bad_column_name number);
+
+alter table bad_table_name
+rename column bad_column_name to good_column_name;
+
+rename bad_table_name to good_table_name;
 
 
-SQL>    ';
-SP2-0042: unknown command "'" - rest of line ignored.
-SQL> begin
-  2     null;
-  3  end;
-  4  /
 
-PL/SQL procedure successfully completed.
 
-SQL>
 
