d76fb93fc6591facfcfbd8c00514f99ca28d5d0c
Jon Heller <jonearles@yahoo.com>
2018-12-10 08:56:37 -0600

    Chapter 17 changes.

--- examples/Chapter 17.sql
+++ examples/Chapter 17.sql
@@ -103,12 +103,97 @@ select * from table(dbms_xplan.display(format => 'basic'));
 -- Cardinality
 ---------------------------------------------------------------------------
 
+--Full table scan example.
+explain plan for select * from launch where site_id = 1895;
 
+select * from table(dbms_xplan.display(format => 'basic +rows'));
+
+
+--Index range scan example.
+explain plan for select * from launch where site_id = 780;
+
+select * from table(dbms_xplan.display(format => 'basic +rows'));
+
+
+
+---------------------------------------------------------------------------
+-- Optimizer Statistics
+---------------------------------------------------------------------------
+
+--Generate an optimizer trace file.
+alter session set events='10053 trace name context forever, level 1';
+select * from space.launch where site_id = 1895;
+alter session set events '10053 trace name context off';
+
+--Find the latest .trc file in this directory:
+select value from v$diag_info where name = 'Diag Trace';
+
+
+--Column statistics for SPACE.LAUNCH.SITE_ID.
+select histogram, sample_size
+from dba_tab_columns
+where owner = 'SPACE'
+	and table_name = 'LAUNCH'
+	and column_name = 'SITE_ID';
+
+
+--Histogram bucket for 1895.
+select endpoint_repeat_count
+from dba_histograms
+where owner = 'SPACE'
+	and table_name = 'LAUNCH'
+	and column_name = 'SITE_ID'
+	and endpoint_value = 1895;
+
+
+
+---------------------------------------------------------------------------
+-- Transformations and Dynamic Optimizations
+---------------------------------------------------------------------------
+
+--Launches in a popular and unpopular year.
+select * from launch join satellite using (launch_id)
+where to_char(launch.launch_date, 'YYYY') = '1970';
+
+select * from launch join satellite using (launch_id)
+where to_char(launch.launch_date, 'YYYY') = '2050';
+
+
+--Find actual execution plan of either 1970 or 2050 query.
+select * from table(dbms_xplan.display_cursor(
+	sql_id =>
+	(
+		select distinct sql_id from gv$sql
+		where sql_fulltext like '%1970%'
+		--where sql_fulltext like '%2050%'
+			and sql_fulltext not like '%quine%'
+	),
+	format => 'adaptive')
+);
+
+
+
+---------------------------------------------------------------------------
+-- Transformations
+---------------------------------------------------------------------------
+
+--Trivial example of predicate pushing.
+select * from (select * from launch) where launch_id = 1;
+
+
+--Simple view merging example.
 select *
-from space.launch;
+from
+(
+	select *
+	from satellite join launch using (launch_id)
+) launch_satellites
+join site
+	on launch_satellites.site_id = site.site_id
+where site.site_id = 1;
 
 
-
-
-
-
+--Simple unnesting example.  (Launches with a satelite.)
+select *
+from launch
+where launch_id in (select launch_id from satellite);
