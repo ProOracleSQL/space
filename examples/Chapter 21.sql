52253797cfd9c19af322d1f39a0d852d284a341c
Jon Heller <jonearles@yahoo.com>
2019-02-15 22:09:52 -0600

    Chapter 21 changes based on author review.

--- examples/Chapter 21.sql
+++ examples/Chapter 21.sql
@@ -24,6 +24,97 @@ end;
 
 
 ---------------------------------------------------------------------------
+-- Transaction control I – COMMIT and ROLLBACK
+---------------------------------------------------------------------------
+
+--Create a simple table for transaction tests.
+create table transaction_test(a number);
+
+
+--Insert one row:
+insert into transaction_test values(1);
+
+--Fails with: ORA-01722: invalid number
+update transaction_test set a = 'B';
+
+--Still contains the original inserted value:
+select * from transaction_test;
+
+
+--Reset the scratch table.
+truncate table transaction_test;
+
+--Combine good INSERT and the bad UPDATE in a PL/SQL block.
+begin
+	insert into transaction_test values(1);
+	update transaction_test set a = 'B';
+end;
+/
+
+--The table is empty:
+select * from transaction_test;
+
+
+--Unnecessary exception handling.
+begin
+	insert into transaction_test values(1);
+	update transaction_test set a = 'B';
+exception when others then
+	rollback;
+	raise;
+end;
+/
+
+
+--Correct: SQL%ROWCOUNT is before ROLLBACK.
+begin
+	insert into transaction_test values(1);
+	dbms_output.put_line('Rows inserted: '||sql%rowcount);
+	rollback;
+end;
+/
+
+--Incorrect: SQL%ROWCOUNT is after ROLLBACK.
+begin
+	insert into transaction_test values(1);
+	rollback;
+	dbms_output.put_line('Rows inserted: '||sql%rowcount);
+end;
+/
+
+
+
+---------------------------------------------------------------------------
+-- Transaction control II – Consistency
+---------------------------------------------------------------------------
+
+
+--Session #1: These commands all run fine.
+truncate table transaction_test;
+
+insert into transaction_test values(0);
+commit;
+
+savepoint savepoint1;
+update transaction_test set a = 1;
+
+--Session #2: This command hangs and waits for locked row.
+update transaction_test set a = 2;
+
+
+--Session #1: Rollback to the previous savepoint.
+--Notice that session #2 is still locked.
+rollback to savepoint1;
+
+
+--Session #3: This command works.  Session #2 is still waiting.
+update transaction_test set a = 3;
+commit;
+
+
+
+
+---------------------------------------------------------------------------
 -- Simple types
 ---------------------------------------------------------------------------
 
@@ -196,6 +287,70 @@ end;
 /
 
 
+--Define, populate, and iterate an associative array.
+declare
+	type string_aat is table of number index by varchar2(4000);
+	v_category_counts string_aat;
+	v_category varchar2(4000);
+begin
+	--Load categories.
+	for categories in
+	(
+		select launch_category, count(*) the_count
+		from launch
+		group by launch_category
+	) loop
+		v_category_counts(categories.launch_category) :=
+			categories.the_count;
+	end loop;
+
+	--Loop through and print categories and values.
+	v_category := v_category_counts.first;
+	while v_category is not null loop
+		dbms_output.put_line(v_category||': '||
+			v_category_counts(v_category));
+		v_category := v_category_counts.next(v_category);
+	end loop;
+end;
+/
+
+
+
+---------------------------------------------------------------------------
+-- Functions
+---------------------------------------------------------------------------
+
+--Calculate orbital period in minutes, based on apogee
+--and perigee in kilometers.  (Only works for Earth orbits.)
+create or replace function get_orbit_period
+(
+	p_apogee number,
+	p_perigee number
+) return number is
+	c_earth_radius constant number := 6378;
+	v_radius_apogee number := p_apogee + c_earth_radius;
+	v_radius_perigee number := p_perigee + c_earth_radius;
+	v_semi_major_axis number :=
+		(v_radius_apogee+v_radius_perigee)/2;
+	v_standard_grav_param constant number := 398600.4;
+	v_orbital_period number := 2*3.14159*sqrt(
+		power(v_semi_major_axis,3)/v_standard_grav_param)/60;
+	--pragma udf;
+begin
+	return v_orbital_period;
+end;
+/
+
+--Orbital periods for satellites.
+select
+	norad_id,
+	orbit_period,
+	round(get_orbit_period(apogee,perigee),2) my_orbit_period
+from satellite
+where perigee is not null
+order by norad_id;
+
+
 
 ---------------------------------------------------------------------------
 -- Table functions
@@ -289,6 +444,121 @@ end;
 
 --Call the function to create the table.
 select test_function from dual;
+
+
+--Autonomous transaction for logging
+--Create simple table to hold application messages.
+create table application_log
+(
+	message  varchar2(4000),
+	the_date date
+);
+
+--Autnomous logging procedure.
+create or replace procedure log_it
+(
+	p_message varchar2,
+	p_the_date date
+) is
+	pragma autonomous_transaction;
+begin
+	insert into application_log
+	values(p_message, p_the_date);
+
+	commit;
+end;
+/
+
+--Sample table.
+create table empty_table(a number);
+
+--Autonomous transaction works despite rollback.
+begin
+	insert into empty_table values(1);
+	log_it('Inserting...', sysdate);
+	rollback;
+end;
+/
+
+--The logging table has the original log message.
+select count(*) from application_log;
+
+--Even though the main transaction was rolled back.
+select count(*) from empty_table;
+
+
+
+---------------------------------------------------------------------------
+-- Triggers
+---------------------------------------------------------------------------
+
+
+--Clear the table.  (NOT SHOWN IN BOOK.)
+delete from transaction_test;
+
+--Create a trigger to track every row change.
+create or replace trigger transaction_test_trg
+after insert or update of a or delete on transaction_test
+for each row
+begin
+	case
+		when inserting then
+			dbms_output.put_line('inserting '||:new.a);
+		when updating('a') then
+			dbms_output.put_line('updating a from '||
+				:old.a||' to '||:new.a);
+		when deleting then
+			dbms_output.put_line('deleting '||:old.a);
+	end case;
+end;
+/
+
+--Test the trigger.
+begin
+	insert into transaction_test values(1);
+	update transaction_test set a = 2;
+	delete from transaction_test;
+end;
+/
+
+
+--Create logon trigger that sets a custom NLS_DATE_FORMAT.
+create or replace trigger jheller.custom_nls_date_format_trg
+after logon on jheller.schema
+begin
+	execute immediate
+		q'[alter session set nls_date_format = 'J']';
+end;
+/
+
+--Logout and logon again and run this to see the new format.
+select to_char(sysdate) julian_day from dual;
+
+JULIAN_DAY
+----------
+   2458530
+
+
+
+
+--Imitation on-commit trigger.
+declare
+	v_job number;
+begin
+	--Create a job, but it won't take effect yet.
+	dbms_job.submit
+	(
+		job  => v_job,
+		what => 'insert into transaction_test values(1);'
+	);
+
+	--A rollback would ignore the job.
+	--rollback;
+
+	--Only a commit will truly create the job.
+	commit;
+end;
+/
 
 
 
